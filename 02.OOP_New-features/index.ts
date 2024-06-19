9573a090262fd68ad8c02b5035e3e9fd95e8c938
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2024-06-19 17:18:10 +0200

    init new section, start with getter

--- 02.OOP_New-features/index.ts
+++ 02.OOP_New-features/index.ts
@@ -1,1 +1,5 @@
-console.log("02.OOP_New-features")
+export const logger = (filename: string, string: string) => {
+  return console.log(`\n \x1b[1m\x1b[34m➜ ${filename.split('02.OOP_New-features/')[1]}\x1b[0m\n\x1b[32m${string}\x1b[0m\n`)
+}
+
+import "./01.getters"
