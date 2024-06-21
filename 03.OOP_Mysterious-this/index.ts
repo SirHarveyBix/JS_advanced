cba93590d083bdf5427e219c112bacce117fb321
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2024-06-21 19:04:05 +0200

    exploring this

--- 03.OOP_Mysterious-this/index.ts
+++ 03.OOP_Mysterious-this/index.ts
@@ -4,3 +4,5 @@ export const logger = (filename: string, string: string) => {
   return console.log(`\n ${TEXT.BOLD}${TEXT.PURPLE}➜ ${filename.split('03.OOP_Mysterious-this/')[1]}${TEXT.CLOSURE}\n${TEXT.GREEN}${string}${TEXT.CLOSURE}\n`)
 }
 
+import "./01.intro"
+import "./02.Mystery-of-this"
