d75d8b77225363a3879681594b33d0f93675e552
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2024-06-24 14:06:53 +0200

    add: bind()

--- 03.OOP_Mysterious-this/07.apply().ts
+++ 03.OOP_Mysterious-this/07.apply().ts
@@ -1,45 +1,45 @@
-import { logger } from './index';
-import { TEXT } from '../utils/colors';
+import { logger } from './index'
+import { TEXT } from '../utils/colors'
 
-logger(__filename, '   The apply() method');
+logger(__filename, '   The apply() method')
 
 const guillaume = {
   firstName: 'Guillaume',
   greet: function (greeting: string, punctuation: string) {
-    console.log(`${TEXT.YELLOW}${TEXT.BOLD}This is${TEXT.CLOSURE}`, this);
-    return `${this?.firstName} says ${greeting}${punctuation}\n`;
-  },
-};
+    console.log(`${TEXT.YELLOW}${TEXT.BOLD}This is${TEXT.CLOSURE}`, this)
+    return `${this?.firstName} says ${greeting}${punctuation}\n`
+  }
+}
 
 const georges = {
   firstName: 'georges',
-  lastName: 'Harrison',
-};
+  lastName: 'Harrison'
+}
 
 console.log(
   "guillaume.greet.call(georges, 'hi') =>",
   guillaume.greet.call(georges, 'hi', '!!!')
-);
+)
 
-console.log('\n   apply() needs an array to work on function parameter:');
+console.log('\n   apply() needs an array to work on function parameter:')
 console.log(
   "guillaume.greet.apply(georges, ['hi']) =>",
   guillaume.greet.apply(georges, ['Yo Bruh', '!! as []'])
-);
+)
 
-const nums = [1, 2, 5, 99, 3, 18];
+const nums = [1, 2, 5, 99, 3, 18]
 
-console.log("'Math.max.apply(null, nums) =>'", Math.max.apply(null, nums));
+console.log("'Math.max.apply(null, nums) =>'", Math.max.apply(null, nums))
 console.log(
   "'Math.max.call(null, nums) =>'",
   `${TEXT.RED}[ERROR]: will throws an error !${TEXT.CLOSURE}, unless you spread it`
-);
+)
 
 function maximum(...argumentsNamesDoesNotMatter: any) {
-  console.log("\nfor the return line, on JS, the course doesn't mention destructuring");
-  console.log(arguments);
-  return Math.max.apply(null, [...arguments]);
+  console.log("\nfor the return line, on JS, the course doesn't mention destructuring")
+  console.log(arguments)
+  return Math.max.apply(null, [...arguments])
 }
 
-maximum(1, 8, 1988);
-maximum(1);
+maximum(1, 8, 1988)
+maximum(1)
---
+++ 03.OOP_Mysterious-this/08.bind().ts
@@ -0,0 +1,47 @@
+import { logger } from './index';
+import { TEXT } from '../utils/colors';
+
+logger(__filename, '   The bind() method');
+
+console.log("   .bind() is permanent it generate a new function.\n")
+
+const guillaume = {
+  name: "Guillaume",
+  city: "Nantes",
+  greet: function (greeting: string, punctuation: string) {
+    console.log(`${TEXT.YELLOW}${TEXT.BOLD}This is${TEXT.CLOSURE}`, this);
+    return `${this?.name} says ${greeting}${punctuation}`;
+  },
+  sing: function () {
+    console.log(`${TEXT.YELLOW}${TEXT.BOLD}This is${TEXT.CLOSURE}`, this);
+    return `${this?.name} Sing `;
+
+  }
+}
+
+const marion = {
+  name: "Marion",
+  city: "Plévenon"
+}
+
+const marionSaysHi = guillaume.greet.call(marion, 'hi', '!!!')
+console.log(" [call]: 'guillaume.greet.call(marion, 'hi')' =>", marionSaysHi)
+console.log(" [call]: Became an expression\n")
+
+const marionSaysYoBruh = guillaume.greet.apply(marion, ['Yo Bruh', '!! as []'])
+console.log(' [apply]: needs an array to work on function parameter:');
+console.log(" [apply]: 'guillaume.greet.apply(marion, ['hi'])' =>", marionSaysYoBruh)
+console.log(" [apply]: Became an expression\n")
+
+
+const marionSaysYo = guillaume.greet.bind(marion, 'Yo Bruh', "!! 'its is bound'")
+console.log(" [bind]: 'guillaume.greet.bind(marion, 'Yo Bruh', '!! its is bound')' =>", marionSaysYo())
+console.log(" [bind]: Became a new function\n")
+
+const sing = guillaume.sing
+sing()
+console.log("Here : 'sing()', will never refers to the initial 'guillaume' object context, you must bind it!")
+
+const singBound = guillaume.sing.bind(guillaume)
+singBound()
+console.log("Here : 'singBound()', will always refers to the initial 'guillaume' object context, because it is bound !\n [bind] : 'const singBound = guillaume.sing.bind(guillaume)'")
