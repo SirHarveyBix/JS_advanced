c2e228c8c5d9b57c38af2dd492b7910c36144bd9
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2024-06-25 10:50:07 +0200

    add: small intro

--- 04.OOP_under-the-hood_prototype_more/01.intro.ts
+++ 04.OOP_under-the-hood_prototype_more/01.intro.ts
@@ -1,4 +1,29 @@
 import { logger } from "."
+import { TEXT } from "../utils/colors"
 
 logger(__filename, "   Intro: OOP Under the Hood")
 
+class Dog {
+  name: string
+  breed: string
+
+  constructor(name: string, breed: string) {
+    this.name = name
+    this.breed = breed
+  }
+
+  break(name: string, breed: string) {
+    return name + " " + breed + " Woofs! "
+  }
+}
+
+console.log(`${TEXT.BOLD}JS OO Under The Hood${TEXT.CLOSURE}`)
+console.log(` - ${TEXT.BOLD}new${TEXT.CLOSURE}`)
+console.log(" - prototypes")
+console.log(" - constructor")
+console.log(" - Object.getPrototypeOf")
+console.log(" - Object.prototypes inheritance")
+
+const dog = new Dog("killer", "hairy")
+
+console.log("\n", dog.break.apply(null, ["Dany", "the dog"]))
