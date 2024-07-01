1c934564b8c9a0dc1c2eb6f8dcfe93b8adc84618
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2024-07-01 17:38:29 +0200

    add promises chaing / callback hell

--- 05.asynchronous/05.promise_chaining.ts
+++ 05.asynchronous/05.promise_chaining.ts
@@ -1,17 +1,67 @@
 import { logger } from "."
 import { TEXT } from "../utils/colors"
 import { POKEMON_BASE_URL } from "../utils/variables"
+import { nextPage_ErrorHandling } from "./06.error_handling"
 
 
-export const nextPage_PromiseChaining = () => {
+export const nextPage_PromiseChaining = async () => {
   logger(__filename, `   Promise Chaining to Flatten code`)
 
-  const url = `${POKEMON_BASE_URL}/1`
-  fetch(url)
-    .then(function (response) {
-      console.log(response.body)
-    })
-    .catch(function (error) {
-      console.error(error)
-    })
+
+  const callbackHell = async () => {
+
+    return fetch(`${POKEMON_BASE_URL}/1`)
+      .then((response1) => {
+        console.log(`${TEXT.RED}${TEXT.BOLD}THIS IS CALLBACK HELL !${TEXT.CLOSURE}`)
+        console.log(" [callbackHell] RESPONSE 1")
+        return fetch(`${POKEMON_BASE_URL}/2`)
+          .then((response2) => {
+            console.log("  [callbackHell] RESPONSE 2")
+            return fetch(`${POKEMON_BASE_URL}/3`)
+              .then((response3) => {
+                console.log("   [callbackHell] RESPONSE 3")
+                return fetch(`${POKEMON_BASE_URL}/4`)
+                  .then((response4) => {
+                    console.log("    [callbackHell] RESPONSE 4, Enough !")
+                  })
+                  .catch((error) => console.error("ERR!", error))
+              })
+              .catch((error) => console.error("ERR!", error))
+          })
+          .catch((error) => console.error("ERR!", error))
+      })
+      .catch((error) => console.error("ERR!", error))
+  }
+
+  const simpleCallback = async () => {
+    return fetch(`${POKEMON_BASE_URL}/1`)
+      .then((response1) => {
+        console.log("\nRESPONSE 1 ", response1.body)
+        return fetch(`${POKEMON_BASE_URL}/2`)
+      })
+      .then((response2) => {
+        console.log("RESPONSE 2 ", response2.body)
+        return fetch(`http://nope.nope`)
+      })
+      .then((response3) => {
+        console.log("RESPONSE 3 ", response3.body)
+        return fetch(`${POKEMON_BASE_URL}/4`)
+      })
+      .then((response4) => {
+        console.log("RESPONSE 4 ", response4.body)
+      })
+      .catch((error) => {
+        console.error("IN THE CATCH! Error: ", error)
+      })
+  }
+
+  await callbackHell()
+  await simpleCallback()
+
+  console.log(`\n• When calling ${TEXT.ITALIC}.then${TEXT.CLOSURE} on a promise, can return ${TEXT.ITALIC}new${TEXT.CLOSURE} promise in callback!`)
+  console.log(`  • Can chain asynchronous operations together with ${TEXT.ITALIC}.then${TEXT.CLOSURE} calls`)
+  console.log(`• Only need ${TEXT.ITALIC}one .catch${TEXT.CLOSURE} at the end - you don't have to catch every promise`)
+
+  nextPage_ErrorHandling()
+
 }
