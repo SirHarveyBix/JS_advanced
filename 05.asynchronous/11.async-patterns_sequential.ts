85ce52f865b8694d096cab4cc5da59da235c7d1c
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2024-07-02 13:31:42 +0200

    add: Async Patterns: Sequential

--- 05.asynchronous/11.async-patterns_sequential.ts
+++ 05.asynchronous/11.async-patterns_sequential.ts
@@ -1,11 +1,45 @@
 import { logger } from "."
-import { TEXT } from "../utils/colors"
 import { POKEMON_BASE_URL } from "../utils/variables"
 import axios from "axios"
-
+import { nextPage_AsyncPatternsPromiseAll } from "./12.async-patterns_Promise.all"
 
 export const nextPage_AsyncPatternsSequential = async () => {
   logger(__filename, `   Async Patterns: Sequential Async Operations`)
 
+  async function waitingRequests() {
+    let results = []
+    axios.get(`${POKEMON_BASE_URL}/1`)
+      .then(({ data }) => {
+        results.push(data.name)
+        console.log("[NOT AWAITED] : REQUEST 1 : FINISHED")
+        return axios.get(`${POKEMON_BASE_URL}/2`)
+      })
+      .then(({ data }) => {
+        console.log("[NOT AWAITED] : REQUEST 2 : FINISHED")
+        results.push(data.name)
+        return axios.get(`${POKEMON_BASE_URL}/3`)
+      })
+      .then(({ data }) => {
+        console.log("[NOT AWAITED] : REQUEST 3 : FINISHED")
+        results.push(data.name)
+      })
+    console.log("\n[NOT AWAITED] : WAITING FOR THE REQUEST TO COMPLETE !\n")
+  }
 
+  console.log()
+
+  async function get3Pokemon() {
+    const { data: response1 } = await axios.get(`${POKEMON_BASE_URL}/1`)
+    console.log("[AWAITED] : REQUEST 1 : FINISHED")
+    const { data: response2 } = await axios.get(`${POKEMON_BASE_URL}/2`)
+    console.log("[AWAITED] : REQUEST 2 : FINISHED")
+    const { data: response3 } = await axios.get(`${POKEMON_BASE_URL}/3`)
+    console.log("[AWAITED] : REQUEST 3 : FINISHED")
+
+    const results = [response1, response2, response3]
+  }
+
+  waitingRequests()
+    .then(async () => await get3Pokemon())
+    .then(async () => await nextPage_AsyncPatternsPromiseAll())
 }
