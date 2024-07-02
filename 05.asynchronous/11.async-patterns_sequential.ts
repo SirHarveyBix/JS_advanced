3846e8fd72e41b9cf7e7e47477f257c342205f86
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2024-07-02 13:01:30 +0200

    add: Async Patterns: Parallel

---
+++ 05.asynchronous/11.async-patterns_sequential.ts
@@ -0,0 +1,11 @@
+import { logger } from "."
+import { TEXT } from "../utils/colors"
+import { POKEMON_BASE_URL } from "../utils/variables"
+import axios from "axios"
+
+
+export const nextPage_AsyncPatternsSequential = async () => {
+  logger(__filename, `   Async Patterns: Sequential Async Operations`)
+
+
+}
