1790f8c299621fb788e81fd81b2ca931e1113674
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2024-07-03 17:35:13 +0200

    init section 8: The Tricky Parts: Scope & Closures

---
+++ 08.tricky_part_scope-closure/01.recap_var-scope.ts
@@ -0,0 +1,6 @@
+import { logger } from "."
+import { TEXT } from "../utils/colors"
+
+logger(__filename, `   Recaping Var & Scope`)
+
+console.log(`${TEXT.YELLOW}${TEXT.BOLD}Scope & closure${TEXT.CLOSURE}`)
