860c57ee211344e135ee6a6a5c23c5b455748fb8
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2024-07-08 19:52:55 +0200

    add clearTimeout

--- 09.timers-Debouncing-Throttling-RequestAnimationFrame/01.setTimeout.ts.ts
+++ 09.timers-Debouncing-Throttling-RequestAnimationFrame/01.setTimeout.ts.ts
@@ -11,5 +11,10 @@ const myTimeout = setTimeout(function () {
   console.log('Times up!');
 }, delay);
 
+const anyLog = () => console.log("you'll never see it on your terminal")
+
+const timeoutToClear = setTimeout(anyLog, delay + delay)
+
+clearTimeout(timeoutToClear)
 
 console.log(`\n${TEXT.YELLOW}Uses cases, shown on .js${TEXT.CLOSURE}\n`);
