6b65e1d76722965c771737f0fd2927f7b226ba1d
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2024-07-10 17:53:45 +0200

    add dice game OOP version

---
+++ 10.functional_programming/13.DiceGame.ts
@@ -0,0 +1,6 @@
+import { logger } from "."
+import { TEXT } from "../utils/colors"
+
+logger(__filename, "   Dice Game Intro")
+
+console.log("Check 13.DiceGame folder, drag n drop .html file into your browser to see the results")
