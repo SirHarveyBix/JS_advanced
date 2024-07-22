26d3568dcc9388b2530dc33bb3ef70c858ea0221
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2024-07-22 14:33:07 +0200

    init section 12

---
+++ 12.web-storage_APIs/01.local-storage.ts
@@ -0,0 +1,9 @@
+
+import { logger } from "."
+import { TEXT } from "../utils/colors"
+import { POKEMON_BASE_URL } from "../utils/variables"
+
+console.log(`\n        ${TEXT.BOLD}${TEXT.YELLOW}Storage${TEXT.CLOSURE}`)
+
+logger(__filename, "   LocalStorage Basics")
+
