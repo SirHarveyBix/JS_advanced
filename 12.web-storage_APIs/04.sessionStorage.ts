3f2a822291a26a27b714444039269b0239ad4dc2
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2024-07-23 12:40:12 +0200

    edit session storage

--- 12.web-storage_APIs/04.sessionStorage.ts
+++ 12.web-storage_APIs/04.sessionStorage.ts
@@ -4,6 +4,8 @@ import { TEXT } from "../utils/colors"
 
 logger(__filename, "   SessionStorage Basics")
 
-console.log("As you may Guess, Sessions storage works only during a session; might be until user is disconnected")
+console.log(" [SessionStorage]: to explore a lil' bit, you must drop 04.SessionStorage/index.html file on your browser\n")
+
+console.log("As you may Guess, Sessions storage works only during a session; until the browser / Tab is Open")
 console.log(`\nwork like ${TEXT.PURPLE}LocalStorage${TEXT.CLOSURE} :`)
 console.log(`Key - value pair with ${TEXT.GREEN}.setItem${TEXT.CLOSURE}(), key with ${TEXT.GREEN}.getItem${TEXT.CLOSURE}()`)
