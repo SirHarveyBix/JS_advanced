f18230e7ee13ba1000514d5f451cbcb24aa7a9a5
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2024-07-23 12:40:23 +0200

    add indexed db

---
+++ 12.web-storage_APIs/05.IndexedDB.ts
@@ -0,0 +1,12 @@
+
+import { logger } from "."
+import { TEXT } from "../utils/colors"
+
+logger(__filename, "   IndexedDB: Enter At Your Own Risk!")
+
+console.log("A low-level API for storing structured data, including large datasets.\n")
+console.log("is a king of Server Side Rendering DB storing")
+console.log('you can see some methods in : 05.IndexedDB/indexedDB.js folder')
+
+console.log("\n[SessionStorage]: to explore a lil' bit, you must drop 05.IndexedDB/index.html file on your browser")
+console.log("  ➜ Then, you can access the Indexed Database in your browser : Appli ➜ Indexed Database")
